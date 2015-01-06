84442ad155ef0d6752b83c81ee831d08692aaaa4
Dominique Orban <dominique.orban@gmail.com>
2015-01-06 17:43:35 -0500

    Travis: ensure gfortran is installed.

--- setup_travis_linux.sh
+++ setup_travis_linux.sh
@@ -7,8 +7,12 @@ sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl
 echo -ne '\n' | ruby -e "$(wget -O- https://raw.github.com/Homebrew/linuxbrew/go/install)"
 
 # Symlink GCC to avoid installing Homebrew GCC.
-ln -s $(which gcc) $HOME/.linuxbrew/bin/gcc-$(gcc -dumpversion |cut -d. -f1,2)
+gccver=$(gcc -dumpversion |cut -d. -f1,2)
+ln -s $(which gcc) $HOME/.linuxbrew/bin/gcc-${gccver}
 ln -s $(which g++) $HOME/.linuxbrew/bin/g++-$(g++ -dumpversion |cut -d. -f1,2)
+
+# Ensure GFORTRAN is available.
+sudo apt-get install gfortran-$(gccver)
 ln -s $(which gfortran) $HOME/.linuxbrew/bin/gfortran-$(gfortran -dumpversion |cut -d. -f1,2)
 
 # Ensure BLAS and LAPACK are available.
