df449c9f05a976067be37e69959694b537955d38
Dominique Orban <dominique.orban@gmail.com>
2015-03-12 14:42:36 -0400

    Add Maros-Meszaros HEAD.

--- maros_meszaros.rb
+++ maros_meszaros.rb
@@ -1,5 +1,15 @@
+# CCPForge requires that svn checkouts be done with --username anonymous.
+# This should be available in Homebrew by default in the near future.
+
+class AnonymousSubversionDownloadStrategy < SubversionDownloadStrategy
+  def quiet_safe_system(*args)
+    super(*args + ["--username", "anonymous"])
+  end
+end
+
 class MarosMeszaros < Formula
   homepage "http://www.cuter.rl.ac.uk/Problems/marmes.shtml"
+  head "http://ccpforge.cse.rl.ac.uk/svn/cutest/marosmeszaros/trunk", :using => AnonymousSubversionDownloadStrategy
   url "ftp://ftp.numerical.rl.ac.uk/pub/cuter/marosmeszaros.tar.gz"
   sha1 "231731ebda8182105dad568443bf99e048bf1af1"
   version "1.0"
