6cf309decd81876ab187ddf3ea36b39238256219
Dominique Orban <dominique.orban@gmail.com>
2017-04-18 16:11:55 -0400

    install in /home/linuxbrew

--- setup_travis_linux.sh
+++ setup_travis_linux.sh
@@ -4,17 +4,22 @@ set -ev
 # Install LinuxBrew.
 sudo apt-get update
 sudo apt-get install build-essential curl git file python-setuptools m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev libgsl0-dev
-echo -ne '\n' | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+
+HOME=/home/linuxbrew  # install linuxbrew here so we can install the glibc bottle
+sudo mkdir $HOME
+sudo chown "$USER:" $HOME
+LINUXBREW=$HOME/.linuxbrew
+git clone https://github.com/Linuxbrew/brew $LINUXBREW
 
 # Symlink GCC to avoid installing Homebrew GCC.
 gccver=$(gcc -dumpversion |cut -d. -f1,2)
-ln -s $(which gcc) $HOME/.linuxbrew/bin/gcc-${gccver}
-ln -s $(which g++) $HOME/.linuxbrew/bin/g++-$(g++ -dumpversion |cut -d. -f1,2)
+ln -s $(which gcc) $LINUXBREW/.linuxbrew/bin/gcc-${gccver}
+ln -s $(which g++) $LINUXBREW/.linuxbrew/bin/g++-$(g++ -dumpversion |cut -d. -f1,2)
 
 # Ensure GFORTRAN is available.
 sudo apt-get -y install gfortran-${gccver}
-ln -s $(which gfortran-${gccver}) $HOME/.linuxbrew/bin/gfortran-${gccver}
-ln -s $(which gfortran-${gccver}) $HOME/.linuxbrew/bin/gfortran
+ln -s $(which gfortran-${gccver}) $LINUXBREW/.linuxbrew/bin/gfortran-${gccver}
+ln -s $(which gfortran-${gccver}) $LINUXBREW/.linuxbrew/bin/gfortran
 
 # Ensure BLAS and LAPACK are available.
 sudo apt-get -y install libblas-dev liblapack-dev
