b2b99061c684c6aa4376ef9e22bbecee60080cdd
Dominique Orban <dominique.orban@gmail.com>
2018-03-01 21:10:10 +0100

    sifdecode: new home

--- sifdecode.rb
+++ sifdecode.rb
@@ -1,18 +1,9 @@
-# CCPForge requires that svn checkouts be done with --username anonymous.
-# This should be available in Homebrew by default in the near future.
-
-class AnonymousSubversionDownloadStrategy < SubversionDownloadStrategy
-  def quiet_safe_system(*args)
-    super(*args + ["--username", "anonymous"])
-  end
-end
-
 class Sifdecode < Formula
   desc "SIF Decoder"
-  homepage "https://ccpforge.cse.rl.ac.uk/gf/project/cutest/wiki"
-  url "https://github.com/optimizers/sifdecode-mirror/archive/v0.4.tar.gz"
-  sha256 "02330c3ba7b024bf8c37bd0bc58c53b2cfb01b8e7446eca502ae231755014fc1"
-  head "https://ccpforge.cse.rl.ac.uk/svn/cutest/sifdecode/trunk", :using => AnonymousSubversionDownloadStrategy
+  homepage "https://github.com/ralna/SIFDecode/wiki"
+  url "https://github.com/ralna/SIFDecode/archive/v2.0.0.tar.gz"
+  sha256 "165640f7b75ff132555157cfafe95dd6dac145f5003fe3ac9d59dcdefa0a22cc"
+  head "https://github.com/ralna/SIFDecode.git"
 
   option "with-pgi", "build with Portland Group compiler"
 
