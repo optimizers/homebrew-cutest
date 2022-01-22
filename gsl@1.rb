b0fa68d226eaa70b7f1947988f84b6210d169fa8
Dominique <dominique.orban@gmail.com>
2022-01-22 01:38:50 +0000

    gsl@1: add 1.16_2 bottle.

--- gsl@1.rb
+++ gsl@1.rb
@@ -6,6 +6,13 @@ class GslAT1 < Formula
   sha256 "73bc2f51b90d2a780e6d266d43e487b3dbd78945dd0b04b14ca5980fe28d2f53"
   revision 2
 
+  bottle do
+    root_url "https://github.com/optimizers/homebrew-cutest/releases/download/gsl@1-1.16_2"
+    sha256 cellar: :any,                 big_sur:      "a709fa1756f5ed57845d60e5cb059b8ebd862facadedcdd5aa5778abc395fc76"
+    sha256 cellar: :any,                 catalina:     "74325edeb3a2d7fcfbffe89f16567955ee2c8a5f4dd1f9784108765c2d4fb3a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7c1d37552bdbf396eb51b583e8cfbaf76b68457a13e345f4d616e820906b3275"
+  end
+
   keg_only :versioned_formula
 
   def install
