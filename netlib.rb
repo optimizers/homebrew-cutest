55d1a30b699bc95979f76b35543764aad5ba4c19
Dominique <dominique.orban@gmail.com>
2022-01-22 01:38:50 +0000

    netlib: add 0.1_3 bottle.

--- netlib.rb
+++ netlib.rb
@@ -7,6 +7,13 @@ class Netlib < Formula
 
   head "https://bitbucket.org/optrove/netlib-lp.git"
 
+  bottle do
+    root_url "https://github.com/optimizers/homebrew-cutest/releases/download/netlib-0.1_3"
+    sha256 cellar: :any_skip_relocation, big_sur:      "fb782247d21b33eec0c490b576a76346442c6f709cc650608b90cc2d4f7b1359"
+    sha256 cellar: :any_skip_relocation, catalina:     "8ec9730fc5754707f510df2f30f4496d1f4a805c3407a1128fc7a7dc01cb2baf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8cccbc3f5255aec8eb7ddc9f17c47d21c76706720cdd285a61ddb8d497b369a3"
+  end
+
   keg_only "this formula only installs data files"
 
   def install
