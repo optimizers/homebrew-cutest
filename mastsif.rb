9625b97949346bafc802a6741a9fc6bb2973bd06
Dominique <dominique.orban@gmail.com>
2022-01-22 01:38:50 +0000

    mastsif: add 0.5_2 bottle.

--- mastsif.rb
+++ mastsif.rb
@@ -7,6 +7,13 @@ class Mastsif < Formula
 
   head "https://bitbucket.org/optrove/sif.git"
 
+  bottle do
+    root_url "https://github.com/optimizers/homebrew-cutest/releases/download/mastsif-0.5_2"
+    sha256 cellar: :any_skip_relocation, big_sur:      "ef83954ba6ddcd0817c4dcb2755abab5177be52ce86ae264d1a4c4fdcc4f1aac"
+    sha256 cellar: :any_skip_relocation, catalina:     "77681296458be8aa77d320b4ab10b5392b566bbb4933f6591004da21bd136711"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "868c58e1e757e0a5e3be1212854dc770a048eaf1a52357d99ac43ecdba38e5b2"
+  end
+
   keg_only "this formula only installs data files"
 
   def install
