366d0e2f401aa4ac672e07c49a73c067d27a7e76
Dominique <dominique.orban@gmail.com>
2022-01-22 01:38:50 +0000

    sifdecode: add 2.0.1_2 bottle.

--- sifdecode.rb
+++ sifdecode.rb
@@ -7,6 +7,13 @@ class Sifdecode < Formula
 
   head "https://github.com/ralna/SIFDecode.git"
 
+  bottle do
+    root_url "https://github.com/optimizers/homebrew-cutest/releases/download/sifdecode-2.0.1_2"
+    sha256 cellar: :any,                 big_sur:      "c781d13ea2ece599788b1caae6151c7401e01cec1f26cb3ad4f8f87175be8e1b"
+    sha256 cellar: :any,                 catalina:     "e3369da43f231fa1e82e9b1245d7ce93527e7d79beed3750ba8853b85d9fda43"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "26b481b4b7b26ef599d4602a5d5019d4b2af2cd904375f485dc5be186a0b1c63"
+  end
+
   depends_on "optimizers/cutest/archdefs" => :build
   depends_on "gcc"
   env :std
