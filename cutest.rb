8aed08e27123fa28e7a2646a44c5e753b86151e7
Dominique <dominique.orban@gmail.com>
2025-09-21 11:14:11 -0400

    cutest: version 2.5.8
    Closes #96.
    
    Signed-off-by: Dominique <dominique.orban@gmail.com>

--- cutest.rb
+++ cutest.rb
@@ -1,9 +1,8 @@
 class Cutest < Formula
   desc "Constrained and Unconstrained Testing Environment on steroids"
   homepage "https://github.com/ralna/CUTEst/wiki"
-  url "https://github.com/ralna/CUTEst/archive/refs/tags/v2.5.7.tar.gz"
-  sha256 "8d8dbb60fcbf7576130e648446d7b401a50c1583693a19f3acc8533602cfda8f"
-  revision 2
+  url "https://github.com/ralna/CUTEst/archive/refs/tags/v2.5.8.tar.gz"
+  sha256 "e01ef55e3ec99fbd8b5a7989fd7e4f234ce3bef239a4685a397cbea640492fb1"
 
   head "https://github.com/ralna/CUTEst.git", branch: "master"
 
@@ -26,24 +25,14 @@ class Cutest < Formula
     meson_args = %w[
       -Dmodules=true
       -Dtests=true
+      -Dquadruple=true
     ]
-    system "meson",
-           "setup",
-           "build",
-           "-Dquadruple=true",
-           *meson_args,
-           *std_meson_args
+    system "meson", "setup", "build", *meson_args, *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
     system "meson", "test", "-C", "build"
 
-    system "meson",
-           "setup",
-           "build_shared",
-           "-Dquadruple=true",
-           "-Ddefault_library=shared",
-           *meson_args,
-           *std_meson_args
+    system "meson", "setup", "build_shared", "-Ddefault_library=shared", *meson_args, *std_meson_args
     system "meson", "compile", "-C", "build_shared", "--verbose"
     system "meson", "install", "-C", "build_shared"
     system "meson", "test", "-C", "build_shared"
